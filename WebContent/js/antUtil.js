fbea50c890840c7334f64708c59b722194de0343
amosleaf <amosleaf@gmail.com>
2012-05-13 14:40:17 +0800

    ajax logout check

--- WebContent/js/antUtil.js
+++ WebContent/js/antUtil.js
@@ -18,31 +18,21 @@ ant.antAjax = function(option) {
 			}
 			return;
 		}
-		if (data.indexOf('"errorId":"session.001"') >= 0
+		if (data.length <= 500 && data.indexOf('"errorId":"session.001"') >= 0
 				&& data.indexOf('"ok":false') >= 0) {
 			top.location = "index/index.jsp";
 		}
 	}
 
 	option.error = function(data) {
-		if (data.ok == false) {
-			if (data.errorId == "session.001") {
-				top.location = "index/index.jsp";
-			}
-			return;
-		}
+		processLogOut(data);
 		if (typeof errorHandler == "function") {
 			var env = option.context || this;
 			errorHandler.call(env, data);
 		}
 	};
 	option.success = function(data) {
-		if (data.ok == false) {
-			if (data.errorId == "session.001") {
-				top.location = "index/index.jsp";
-			}
-			return;
-		}
+		processLogOut(data);
 		if (typeof successHandler == "function") {
 			var env = option.context || this;
 			successHandler.call(env, data);
